c46847b9395698ec944d7c63e773d2be55d10604
Farhan Syahrul Fath <farhan.sf2002@gmail.com>
2024-11-04 16:32:21 +0700

    first commit

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -5,10 +5,10 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
   const user = await currentUser()
-
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
+  if(userInfo?.onboarded) redirect('/')
 
   const userData = {
     id: user.id,
--- src/components/shared/ProfileHeader.tsx
+++ src/components/shared/ProfileHeader.tsx
@@ -7,10 +7,11 @@ interface Props {
   username: string
   imgUrl: string
   bio: string
+  type?: 'User' | 'Community'
 }
 
 export default function ProfileHeader({
-  accountId, authUserId, name, username, imgUrl, bio
+  accountId, authUserId, name, username, imgUrl, bio, type
 }: Props) {
   return (
     <div className="flex w-full flex-col">
